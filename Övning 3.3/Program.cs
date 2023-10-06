13e2ccf7f907a7f7aeb3b9a8b46b7db205268390
Admin <Admin@TEB123013>
2023-10-06 14:56:10 +0200

    Add project files.

---
+++ Övning 3.3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace Övning_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad heter du?");
+            string namn = Console.ReadLine().ToLower();
+
+            if (namn == "stig")
+            {
+                Console.WriteLine("Du har namnsdag idag");
+            }
+            else if (namn == "abraham") 
+            {
+                Console.WriteLine("Du har namnsdag imorgon"); 
+            }
+            else
+            {
+                Console.WriteLine("Du har inte namnsdag idag eller imorgon");
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
